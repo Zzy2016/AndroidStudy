970f14c370fe1f0f6d87f091c1a372d4cfec619d
zhangzeyang <zhangjie@huidr.com>
2020-11-03 18:43:39 +0800

    DataBinding 1

--- databindtest/src/main/java/com/example/databindtest/MainActivity.java
+++ databindtest/src/main/java/com/example/databindtest/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.databindtest.databinding.ActivityMainBindingImpl;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    User user;
+
+    ActivityMainBindingImpl activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        user = new User("xiaoming", "daming");
+
+        activityMainBinding = ActivityMainBindingImpl.inflate(getLayoutInflater());
+
     }
 }
---
+++ databindtest/src/main/java/com/example/databindtest/User.java
@@ -0,0 +1,14 @@
+package com.example.databindtest;
+
+/**
+ * @author: Administrator
+ * @date: 2020/11/3
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
