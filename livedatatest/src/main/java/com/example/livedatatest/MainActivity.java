d529b4cd779c4082a0afcd2426ff80a17b1d88e6
zhangzeyang <zhangjie@huidr.com>
2020-11-06 18:30:06 +0800

    LifeCycle

--- livedatatest/src/main/java/com/example/livedatatest/MainActivity.java
+++ livedatatest/src/main/java/com/example/livedatatest/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+//LiveData 是一种可观察的数据存储器类。与常规的可观察类不同，LiveData 具有生命周期感知能力，
+//        意指它遵循其他应用组件（如 Activity、Fragment 或 Service）的生命周期。这种感知能
+//        力可确保 LiveData 仅更新处于活跃生命周期状态的应用组件观察者。
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
