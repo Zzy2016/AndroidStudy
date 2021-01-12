531a742f4d8602e30e9f01704c7bcbb137f6c822
zhangzeyang <zhangzeyang@huidr.com>
2021-01-12 19:08:23 +0800

    '添加图片'

--- app/src/main/java/com/example/androidstudy/base/BaseActivity.java
+++ app/src/main/java/com/example/androidstudy/base/BaseActivity.java
@@ -1,7 +1,14 @@
 package com.example.androidstudy.base;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.ViewDataBinding;
 
-public abstract class BaseActivity<DB extends ViewDataBinding,VM extends BaseViewModel> {
+public abstract class BaseActivity<DB extends ViewDataBinding,VM extends BaseViewModel> extends AppCompatActivity {
+
+    public DB mDataBinding;
+    protected VM mViewModel;
+
+
+
 
 }
